1040366055c6e592190b9f630f5f26b79da76f7a
sunjoon-kim <sunjun7788@naver.com>
2024-05-08 17:14:33 +0900

    first commit

--- Calculator.java
+++ Calculator.java
@@ -6,18 +6,18 @@ public class Calculator {
     System.out.println("===Java Calculator===");
 
 
-    Scanner input = new Scanner(System.in);       // 스캐너 공부 다시 스캐너 이름 input
+    Scanner input = new Scanner(System.in);       
    
-      System.out.println("숫자 하나를 입력하시오.");    //3번 입력 숫자 , 연산자 , 숫자 형식
+      System.out.println("숫자 하나를 입력하시오.");   
       double firstNum = input.nextDouble();
 
       System.out.println("연산자 +,-,*,/ 중에 입력하시오.");
-      String sign = input.next();              //들어오는 문자열이고 혹은 char sign = input.next().chatAt(0);
+      String key = input.next();            
 
       System.out.println("숫자 하나를 입력하시오.");
       double secondNum = input.nextDouble();
 
-       switch (sign){
+       switch (key){
            case  "+" :
            System.out.println(firstNum + "+" + secondNum + "=" + (firstNum + secondNum));
            break;
@@ -35,40 +35,3 @@ public class Calculator {
            break;
        }
        input.close();
-
-       /*
-
-            1. 사용자의 입력으로 첫 번째 숫자, 연산자, 두 번째 숫자를 받아야 합니다.
-              -숫자 + 연산자 + 숫자
-            2. 연산자의 종류는 +, -, *, / 입니다.
-            3. 소수점 연산을 수행할 수 있어야 합니다.
-              -double 타입으로~
-            4. 연산 결과를 콘솔에 출력합니다.
-             -출력
-
-        */
-    // TODO ... 다른 방법으로 계산기 만들어보기
-
-//     this.sum = new Array();
-
-//     onkeydown(key){
-
-//     if(key.num == '/'|key.num == '*'||key.num == '+'||key.num == '-'|)  
-//       this.sum.push(this.led.text);
-//       this.sum.text='';
-//       this.sum.push(key.num);
-//   }else if(key.num == '='){
-//       this.sum.push(this.led.text);
-//       var sumtext='';
-//       this.sum,forEach(element => {
-//           sumtext+=element;
-//       }); 
-//       this.led.text=eval(sumtext);
-//       this.sum = new Array(); 
-//   }else if(key.num == 'C'){
-//       this.led.text='';
-//       this.sum = new Array();  
-//   }else{
-//       this.led.text+=key.num;
-//   }
-// }
